f3f21438efbd38cec64d5f7fe2c2b6bfcb93ae17
Aleksandra Porada <aleksandra.porada92@gmail.com>
2021-03-24 21:18:46 +0100

    Issue with creating movies. Will try again tomorrow

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,23 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => res.render("index"));
+
+/* GET movies page */
+const Movie = require("../models/Movie.model");
+
+router.get("/movies", (req, res) => {
+  Movie.find()
+    .then((allBooks) =>
+      console.log("We've got the books! We're off the hooks!", allBooks)
+    )
+    .catch((err) => {
+      console.log(
+        "Error happened while getting the books. Not like reading much!",
+        err
+      );
+    });
+});
 
 module.exports = router;
